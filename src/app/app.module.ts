1a4921be39f0550f226925b325c28bbaebd6e280
Saeed Khamseh <saeed.khamseh67@gmail.com>
2023-03-05 12:31:58 +0330

    Create basic layout and input fields for customer form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,20 @@ import {MatDialogModule} from "@angular/material/dialog";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatIconModule} from "@angular/material/icon";
+import {DateAdapter, MatNativeDateModule, NativeDateAdapter} from "@angular/material/core";
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldDefaultOptions} from "@angular/material/form-field";
 
 @NgModule({
   bootstrap: [AppComponent],
   declarations: [AppComponent, CustomerComponent, CustomerListComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatTableModule, MatDialogModule, MatInputModule, MatButtonModule],
-  providers: []
+  imports: [BrowserModule, BrowserAnimationsModule, MatTableModule, MatDialogModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatIconModule,
+    MatNativeDateModule],
+  providers: [
+    {provide: DateAdapter, useClass: NativeDateAdapter},
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: <MatFormFieldDefaultOptions>{appearance: 'outline', hideRequiredMarker: true}},
+  ]
 })
 export class AppModule {
 }
--- src/app/components/customer-list/customer-list.component.ts
+++ src/app/components/customer-list/customer-list.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {MatDialog} from "@angular/material/dialog";
 import {CustomerComponent} from "../customer/customer.component";
 
 export interface Customer {
